6de12c194618f8ed7e6ad634aed3c1705cc2270c
p.belezov <p.belezov@mfc38.ru>
2024-01-15 14:10:19 +0800

    Removed testing echo

--- src/Esia/SignerCPDataHash.php
+++ src/Esia/SignerCPDataHash.php
@@ -26,7 +26,6 @@ class SignerCPDataHash extends AbstractSignerPKCS7 implements SignerInterface
         $store = new \CPStore();
         $store->Open(CURRENT_USER_STORE, 'My', STORE_OPEN_READ_ONLY); // используем хранилище My текущего пользователя (www-data)
         $certs = $store->get_Certificates();
-        echo $this->config->getClientId();
         $certlist = $certs->Find(CERTIFICATE_FIND_SUBJECT_NAME, $this->config->getClientId(), 0); // ищем сертификат, у которогое Subject = мнемонике нашей ИС
         $cert = $certlist->Item(1);
         if (!$cert) {
